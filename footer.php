c732ed70e3ecbdd7c238c7e3806ca9ce6019add0
Almer Lalic <lalicalmer@gmail.com>
2025-12-03 21:21:23 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,18 +1,28 @@
-<div class="section" style="margin-top: 40px;">
-    <h2>Come Visit Us</h2>
-    <div class="contact-details">
-        <p>We'd love to see you in person and help you find your next great read!</p>
-        <p><strong>Our location:</strong> <?php echo $store_address; ?></p>
-        <p><strong>Give us a call:</strong> <?php echo $store_phone; ?></p>
-        <p><strong>Send us an email:</strong> <?php echo $store_email; ?></p>
-        <p><strong>When we're open:</strong> <?php echo $store_hours; ?></p>
+        
+<div class="site-footer">
+            
+    <div class="quick-links">
+        <h3>Quick Navigation</h3>
+        <?php foreach($nav_links as $title => $link): ?>
+            <a href="<?php echo $link; ?>" class="quick-link">
+                <?php echo $title; ?>
+            </a>
+        <?php endforeach; ?>
     </div>
-    
-    <div style="margin-top: 30px; padding-top: 20px; border-top: 1px solid #e8e8e8;">
-        <p style="color: #666; font-size: 0.95em;">
-            Thank you for visiting The Reading Nook today. 
-            We hope you found something that caught your eye!<br>
-            <em>Page updated on <?php echo date('F j, Y'); ?> at <?php echo date('g:i A'); ?></em>
-        </p>
+    <div class="contact-details">
+        <h3>Come Visit Us</h3>
+        <p> We'd love to see you in person and help you find your next great read! </p>
+        <p><strong> Our location: </strong> <?php echo $store_address; ?></p>
+        <p><strong> Give us a call: </strong> <?php echo $store_phone; ?></p>
+        <p><strong> Send us an email: </strong> <a href="mailto:<?php echo $store_email; ?>"><?php echo $store_email; ?></a></p>
+        <p><strong> When we're open: </strong> <?php echo $store_hours; ?></p>
+    </div>
+            
+            
+    <div class="copyright">
+        <p>&copy; <?php echo date('Y'); ?> <?php echo $store_name; ?></p>
     </div>
 </div>
+</div> 
+</body>
+</html>
