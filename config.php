97e5df8d7611ad488f881e07071b29723d9a4d3c
Almer Lalic <lalicalmer@gmail.com>
2025-12-03 21:19:41 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,28 +1,14 @@
-        
-<div class="site-footer">
-            
-    <div class="quick-links">
-        <h3>Quick Navigation</h3>
-        <?php foreach($nav_links as $title => $link): ?>
-            <a href="<?php echo $link; ?>" class="quick-link">
-                <?php echo $title; ?>
-            </a>
-        <?php endforeach; ?>
-    </div>
-    <div class="contact-details">
-        <h3>Come Visit Us</h3>
-        <p> We'd love to see you in person and help you find your next great read! </p>
-        <p><strong> Our location: </strong> <?php echo $store_address; ?></p>
-        <p><strong> Give us a call: </strong> <?php echo $store_phone; ?></p>
-        <p><strong> Send us an email: </strong> <a href="mailto:<?php echo $store_email; ?>"><?php echo $store_email; ?></a></p>
-        <p><strong> When we're open: </strong> <?php echo $store_hours; ?></p>
-    </div>
-            
-            
-    <div class="copyright">
-        <p>&copy; <?php echo date('Y'); ?> <?php echo $store_name; ?></p>
-    </div>
-</div>
-</div> 
-</body>
-</html>
+<?php
+$store_name = "The BookStore";
+$store_email = "lalicalmer@gmail.com";
+$store_phone = "0969-370-9820";
+$store_address = "Holy Angel University";
+$store_hours = "Monday - Friday: 9AM - 7PM, Saturday: 10AM - 6PM, Sunday: 11AM - 5PM";
+$special_offer = "Cozy Reading Season - 25% Off All Fiction Books";
+$owner = "Almer Lalic";
+
+$nav_links = [
+    'Home' => 'index.php',
+    'Stock Control' => 'stock-monitor.php'
+];
+?>
